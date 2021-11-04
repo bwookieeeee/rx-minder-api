4a3cac4d7ebc2485409200ecd5b6f74ab7d57b05
Brooke Morrison <5741965+bwookieeeee@users.noreply.github.com>
2021-11-03 23:13:42 -0700

    db-ify users routes

--- routes/user.js
+++ routes/user.js
@@ -1,79 +1,100 @@
 const router = require("express").Router();
 const uuid = require("uuid");
+const Pool = require("pg").Pool;
 
-
-// This is a temporary solution until a database is added:
-let users = require("../dummy/users.json").users;
+const pool = new Pool({
+  user: "rxminder",
+  password: "password",
+  database: "rxminder",
+  host: "127.0.0.1",
+})
 
 router.get("/", (req, res) => { res.sendStatus(405) })
 router.patch("/", (req, res) => { res.sendStatus(405) })
 router.delete("/", (req, res) => { res.sendStatus(405) })
 
-router.get("/:username", (req, res) => {
+router.get("/:username", async (req, res) => {
   const { username } = req.params;
   console.debug(`GET /users/${username}`)
-  const user = users.find(usr => usr.username === username);
-  if (user) {
-    res.status(200).send(user);
-  } else {
-    res.status(404).send({ error: "User does not exist" });
-  }
+
+  await pool.query("SELECT * FROM users WHERE username=$1 LIMIT 1", [username])
+    .then(results => {
+      if (results.rowCount > 0) {
+        res.status(200).send(results.rows[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 })
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   console.debug(`POST /users`);
-  try {
-    if (users.find(usr => usr.username === req.body.username)) {
-      res.status(400).send({ error: "User already exists" });
-    } else {
-      let user = req.body;
-      user.id = uuid.v4();
-      user.apiKey = uuid.v4();
-      users.push(user);
-      res.status(201).send(user);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
-  }
+  const { username, passwdHash, email, firstName, lastName, linkedRxs, linkedReminders } = req.body;
+  await pool.query("INSERT INTO users (id, username, passwdHash, apiKey, email, firstName, lastName, linkedRxs, linkedReminders) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+    [uuid.v4(), username, passwdHash, uuid.v4(), email, firstName, lastName, linkedRxs, linkedReminders])
+    .then(results => {
+      res.status(201).send(results.rows[0]);
+    }).catch(err => {
+      if (err.code === "23505") {
+        res.status(400).send({ error: "User already exists" });
+      } else {
+        console.error(err);
+        res.sendStatus(500);
+      }
+    })
 })
 
-router.patch("/:username", (req, res) => {
+router.patch("/:username", async (req, res) => {
   const { username } = req.params;
   const { passwdHash, email, firstName, lastName, linkedReminders, linkedRxs } = req.body;
   console.debug(`PATCH /users/${username}`)
-  let target = users.find(usr => usr.username === username);
-  if (target) {
-    const targetIdx = users.indexOf(target);
-    const updated = {
-      id: target.id,
-      username: target.username,
-      passwdHash: passwdHash ? passwdHash : target.passwdHash,
-      apiKey: target.apiKey,
-      email: email ? email : target.email,
-      firstName: firstName ? firstName : target.firstName,
-      lastName: lastName ? lastName : target.lastName,
-      linkedRxs: linkedRxs ? linkedRxs : target.linkedRxs,
-      linkedReminders: linkedReminders ? linkedReminders : target.linkedReminders
-    }
 
-    users[targetIdx] = updated;
-    res.status(200).send(updated);
-  } else {
-    res.sendStatus(404);
-  }
+  await pool.query("SELECT * FROM users WHERE username=$1 LIMIT 1", [username])
+    .then(async results => {
+      if (results.rowCount > 0) {
+        const tmpUser = results.rows[0]
+        await pool.query("UPDATE users SET (passwdHash, email, firstName, lastName, linkedRxs, linkedReminders) = ($1, $2, $3, $4, $5, $6) where username = $7 RETURNING *",
+          [
+            passwdHash ? passwdHash : tmpUser.passwdhash,
+            email ? email : tmpUser.email,
+            firstName ? firstName : tmpUser.firstname,
+            lastName ? lastName : tmpUser.lastname,
+            linkedRxs ? linkedRxs : tmpUser.linkedrxs,
+            linkedReminders ? linkedReminders : tmpUser.linkedreminders,
+            username
+          ])
+          .then(results => {
+            res.status(200).send(results.rows[0]);
+          })
+          .catch(err => {
+            console.error(err);
+            res.sendStatus(500);
+          })
+      } else {
+        res.sendStatus(404);
+      }
+    }).catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    })
 })
 
-router.delete("/:username", (req, res) => {
+router.delete("/:username", async (req, res) => {
   const { username } = req.params;
   console.debug(`DELETE /users/${username}`)
-  const target = users.find(usr => usr.username === username);
-  if (target) {
-    users.splice(users.indexOf(target), 1);
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(404);
-  }
+  await pool.query("DELETE FROM users WHERE username=$1 returning *", [username])
+    .then(results => {
+      // There's nothing in results that indicates a 404...
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    })
 })
 
 module.exports = router;
