c26960af3db24a3e78d6e839c3ed32e828c3fb87
Brooke Morrison <5741965+bwookieeeee@users.noreply.github.com>
2021-11-04 13:43:28 -0700

    db-ify scrips routes

--- routes/scrip.js
+++ routes/scrip.js
@@ -1,76 +1,94 @@
 const router = require("express").Router();
 const uuid = require("uuid");
+const Pool = require("pg").Pool;
 
-let scrips = require("../dummy/scrips.json").scrips;
+const pool = new Pool({
+  user: "rxminder",
+  password: "password",
+  host: "127.0.0.1"
+});
 
 router.get("/", (req, res) => { res.sendStatus(405) });
 router.patch("/", (req, res) => { res.sendStatus(405) });
 router.delete("/", (req, res) => { res.sendStatus(405) });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   console.debug(`GET /scrips/${id}`);
-
-  const target = scrips.find(scr => scr.id === id);
-  if (target) {
-    res.status(200).send(target);
-  } else {
-    res.sendStatus(404);
-  }
+  await pool.query("SELECT * FROM scrips WHERE id=$1 LIMIT 1", [id])
+    .then(results => {
+      if (results.rowCount > 0) {
+        res.status(200).send(results.rows[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    }).catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    })
 })
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  const { rxNum, name, strength, stock, instructions, warnings } = req.body;
   console.debug(`POST /scrips`);
-
-  try {
-    if (scrips.find(scr => scr.rxNum === rxNum)) {
-      res.status(400).send({ error: "Scrip already exists" });
-    } else {
-      let scrip = req.body;
-      scrip.id = uuid.v4();
-      scrips.push(scrip);
-      res.status(201).send(scrip);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
-  }
+  await pool.query("INSERT INTO scrips (id, rxnum, name, strength, stock, instructions, warnings) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+    [uuid.v4(), rxNum, name, strength, stock, instructions, warnings])
+    .then(results => {
+      res.status(201).send(results.rows[0]);
+    })
+    .catch(err => {
+      if (err.code === "23505") {
+        res.status(400).send({ error: "scrip already exists" });
+      }
+      console.error(err);
+      res.sendStatus(500);
+    })
 })
 
-router.patch("/:id", (req, res) => {
+router.patch("/:id", async (req, res) => {
   const { id } = req.params;
   const { name, strength, stock, instructions, warnings } = req.body;
   console.debug(`PATCH /scrips/${id}`);
 
-  let target = scrips.find(scr => scr.id === id);
-  if (target) {
-    const targetIdx = scrips.indexOf(target);
-    const updated = {
-      id: target.id,
-      rxNum: target.rxNum,
-      name: name ? name : target.name,
-      strength: strength ? strength : target.strength,
-      stock: stock ? stock : target.stock,
-      instructions: instructions ? instructions : target.instructions,
-      warnings: warnings ? warnings : target.warnings
-    }
-    scrips[targetIdx] = updated;
-    res.status(200).send(updated);
-  } else {
-    res.sendStatus(404);
-  }
+  await pool.query("SELECT * FROM scrips WHERE id=$1", [id])
+    .then(async results => {
+      if (results.rowCount > 0) {
+        const target = results.rows[0];
+        await pool.query("UPDATE scrips SET (name, strength, stock, instructions, warnings) = ($1, $2, $3, $4, $5) RETURNING *", [
+          name ? name : target.name,
+          strength ? strength : target.strength,
+          stock ? stock : target.stock,
+          instructions ? instructions : target.instructions,
+          warnings ? warnings : target.warnings
+        ])
+          .then(results => {
+            res.status(200).send(results.rows[0]);
+          }).catch(err => {
+            console.error(err);
+            res.sendStatus(500);
+          })
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    })
+
 })
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   console.debug(`DELETE /scrips/${id}`);
-  const target = scrips.find(scr => scr.id === scr.id);
-  if (target) {
-    scrips.splice(scrips.indexOf(target), 1);
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(404);
-  }
+  await pool.query("DELETE FROM scrips WHERE id=$1", [id])
+    .then(results => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    })
 })
 
 module.exports = router;
