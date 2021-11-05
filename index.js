f71794879d70012f1d5c4f09b9663316282456ee
Brooke Morrison <5741965+bwookieeeee@users.noreply.github.com>
2021-11-04 20:28:11 -0700

    add access controls headers to appease CORS

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://brookebot.xyz");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+})
 
 app.get("/", (req, res) => {
   const { version } = require("./package.json");
