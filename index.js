e4deb1f46f7df81a16478909afca2fe50198303b
Brooke Morrison <5741965+bwookieeeee@users.noreply.github.com>
2021-11-07 11:33:20 -0800

    add /login route

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const passport = require("passport-local");
+
 
 const app = express();
 
@@ -12,6 +14,29 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   const { version } = require("./package.json");
   res.status(200).send({ version: version })
+})
+
+app.get("/login", async (req, res) => {
+  const Pool = require("pg").Pool;
+  const pool = new Pool({ host: "127.0.0.1", port: 5432, user: "rxminder", password: "password", database: "rxminder" });
+  const bcrypt = require("bcrypt");
+  const authsplit = req.headers.authorization.split(" ")
+  const decoded = Buffer.from(authsplit[1], "base64").toString("ascii");
+  const decodesplit = decoded.split(":")
+  const results = await pool.query("SELECT * FROM users WHERE username=$1", [decodesplit[0]])
+  try {
+
+    bcrypt.compare(decodesplit[1], results.rows[0].passwdhash, (err, result, query=results) => {
+      if (err) {
+        console.error(err);
+        res.status(401).send({ error: "invalid username or password" });
+      } else {
+        res.status(200).send(query.rows[0]);
+      }
+    })
+  } catch (err) {
+    res.sendStatus(401);
+  }
 })
 
 app.use("/users", require("./routes/users"));
